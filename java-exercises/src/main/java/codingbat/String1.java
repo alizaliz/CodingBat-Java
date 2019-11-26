6f867b6cbce16ed5e2b0d1047d2d88982178e39a
Zain Ali <zain_ali8@hotmail.com>
2019-11-26 23:10:46 +1300

    First 7 exercises and test examples

---
+++ java-exercises/src/main/java/codingbat/String1.java
@@ -0,0 +1,99 @@
+package codingbat;
+
+/**
+ * Basic string problems -- no loops. Use + to combine Strings, str.length() is the number of chars in a String, 
+ * str.substring(i, j) extracts the substring starting at index i and running up to but not including index j.
+ */
+public final class String1 {
+
+  /**
+   * Given a string name, e.g. "Bob", return a greeting of the form "Hello Bob!".
+   * 
+   * helloName("Bob") → "Hello Bob!"
+   * helloName("Alice") → "Hello Alice!"
+   * helloName("X") → "Hello X!" 
+  */
+  public String helloName(String name) {
+    return String.format("Hello %s!", name);
+  }
+  
+  /**
+   * Given two strings, a and b, return the result of putting them together in the order abba,
+   * e.g. "Hi" and "Bye" returns "HiByeByeHi".
+   * 
+   * makeAbba("Hi", "Bye") → "HiByeByeHi"
+   * makeAbba("Yo", "Alice") → "YoAliceAliceYo"
+   * makeAbba("What", "Up") → "WhatUpUpWhat"
+   */
+  public String makeAbba(String a, String b) {
+    return a + b + b + a;
+  }
+
+  /**
+   * The web is built with HTML strings like "<i>Yay</i>" which draws Yay as italic text. 
+   * In this example, the "i" tag makes <i> and </i> which surround the word "Yay". 
+   * Given tag and word strings, create the HTML string with tags around the word, e.g. "<i>Yay</i>".
+   * 
+   * makeTags("i", "Yay") → "<i>Yay</i>"
+   * makeTags("i", "Hello") → "<i>Hello</i>"
+   * makeTags("cite", "Yay") → "<cite>Yay</cite>"
+   */
+  public String makeTags(String tag, String word) {
+    return String.format("<%1$s>%2$s</%1$s>", tag, word);
+  }
+
+  /**
+   * Given an "out" string length 4, such as "<<>>", and a word, 
+   * return a new string where the word is in the middle of the out string, e.g. "<<word>>".
+   * Note: use str.substring(i, j) to extract the String starting at index i and going up to but not including index j.
+   * 
+   * makeOutWord("<<>>", "Yay") → "<<Yay>>"
+   * makeOutWord("<<>>", "WooHoo") → "<<WooHoo>>"
+   * makeOutWord("[[]]", "word") → "[[word]]"
+   */
+  public String makeOutWord(String out, String word) {
+    return out.substring(0,out.length()/2) + word + out.substring(out.length()/2);
+  }
+  
+  /**
+   * Given a string, return a new string made of 3 copies of the last 2 chars of the original string.
+   * The string length will be at least 2.
+   * 
+   * extraEnd("Hello") → "lololo"
+   * extraEnd("ab") → "ababab"
+   * extraEnd("Hi") → "HiHiHi"
+   */
+  public String extraEnd(String str) {
+    String end = str.substring(str.length() -2);
+    return String.format("%s%<s%<s", end);
+  }
+
+  /**
+   * Given a string, return the string made of its first two chars, so the String "Hello" yields "He". 
+   * If the string is shorter than length 2, return whatever there is, so "X" yields "X", and the empty string "" yields the empty string "". 
+   * Note that str.length() returns the length of a string.
+   * 
+   * firstTwo("Hello") → "He"
+   * firstTwo("abcdefg") → "ab"
+   * firstTwo("ab") → "ab"
+   */
+  public String firstTwo(String str) {
+    if(str.length() <= 2)
+    {
+      return str;
+    }
+    return str.substring(0,2);
+  }
+
+  /**
+   * Given a string of even length, return the first half. So the string "WooHoo" yields "Woo".
+   * 
+   * firstHalf("WooHoo") → "Woo"
+   * firstHalf("HelloThere") → "Hello"
+   * firstHalf("abcdef") → "abc"
+   */
+  public String firstHalf(String str) {
+    return str.substring(0,str.length()/2);
+  }
+  
+}
---
+++ java-exercises/src/test/java/codingbat/String1Test.java
@@ -0,0 +1,103 @@
+package codingbat;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Test cases for String1 exercises
+ */
+public class String1Test{
+    
+    @Test
+    public void helloNameTest() {
+        String1 testObject = new String1();
+        assertEquals("Hello Bob!",testObject.helloName("Bob"));
+        assertEquals("Hello Alice!",testObject.helloName("Alice"));
+        assertEquals("Hello X!",testObject.helloName("X"));
+        assertEquals("Hello Dolly!",testObject.helloName("Dolly"));
+        assertEquals("Hello Alpha!",testObject.helloName("Alpha"));
+        assertEquals("Hello Omega!",testObject.helloName("Omega"));
+        assertEquals("Hello Goodbye!",testObject.helloName("Goodbye"));
+        assertEquals("Hello ho ho ho!",testObject.helloName("ho ho ho"));
+        assertEquals("Hello xyz!",testObject.helloName("xyz"));
+        assertEquals("Hello Hello!",testObject.helloName("Hello"));
+
+    }
+
+    @Test
+    public void makeAbbaTest()
+    {
+        String1 testObject = new String1();
+        assertEquals("HiByeByeHi",testObject.makeAbba("Hi", "Bye"));
+        assertEquals("YoAliceAliceYo",testObject.makeAbba("Yo", "Alice"));
+        assertEquals("WhatUpUpWhat",testObject.makeAbba("What", "Up"));
+        assertEquals("aaabbbbbbaaa",testObject.makeAbba("aaa", "bbb"));
+        assertEquals("xyyx",testObject.makeAbba("x", "y"));
+        assertEquals("xx",testObject.makeAbba("x", ""));
+        assertEquals("yy",testObject.makeAbba("", "y"));
+        assertEquals("BoYaYaBo",testObject.makeAbba("Bo", "Ya"));
+        assertEquals("YaYaYaYa",testObject.makeAbba("Ya", "Ya"));
+    }
+
+    @Test
+    public void makeTagsTest()
+    {
+        String1 testObject = new String1();
+        assertEquals("<i>Yay</i>",testObject.makeTags("i", "Yay"));
+        assertEquals("<i>Hello</i>",testObject.makeTags("i", "Hello"));
+        assertEquals("<cite>Yay</cite>",testObject.makeTags("cite", "Yay"));
+        assertEquals("<address>here</address>",testObject.makeTags("address", "here"));
+        assertEquals("<body>Heart</body>",testObject.makeTags("body", "Heart"));
+        assertEquals("<i>i</i>",testObject.makeTags("i", "i"));
+        assertEquals("<i></i>",testObject.makeTags("i", ""));
+    }
+
+    @Test
+    public void makeOutWordTest()
+    {
+        String1 testObject = new String1();
+        assertEquals("<<Yay>>",testObject.makeOutWord("<<>>", "Yay"));
+        assertEquals("<<WooHoo>>",testObject.makeOutWord("<<>>", "WooHoo"));
+        assertEquals("[[word]]",testObject.makeOutWord("[[]]", "word"));
+        assertEquals("HHHellooo",testObject.makeOutWord("HHoo", "Hello"));
+        assertEquals("abYAYyz",testObject.makeOutWord("abyz", "YAY"));
+    }
+
+    @Test
+    public void extraEndTest()
+    {
+        String1 testObject = new String1();
+        assertEquals("lololo",testObject.extraEnd("Hello"));
+        assertEquals("ababab",testObject.extraEnd("ab"));
+        assertEquals("HiHiHi",testObject.extraEnd("Hi"));
+        assertEquals("dydydy",testObject.extraEnd("Candy"));
+        assertEquals("dedede",testObject.extraEnd("Code"));
+    }
+
+    @Test
+    public void firstTwoTest()
+    {
+        String1 testObject = new String1();
+        assertEquals("He",testObject.firstTwo("Hello"));
+        assertEquals("ab",testObject.firstTwo("abcdefg"));
+        assertEquals("ab",testObject.firstTwo("ab"));
+        assertEquals("a",testObject.firstTwo("a"));
+        assertEquals("",testObject.firstTwo(""));
+        assertEquals("Ki",testObject.firstTwo("Kitten"));
+        assertEquals("hi",testObject.firstTwo("hi"));
+        assertEquals("hi",testObject.firstTwo("hiya"));
+    }
+    
+    @Test
+    public void firstHalfTest()
+    {
+        String1 testObject = new String1();
+        assertEquals("Woo",testObject.firstHalf("WooHoo"));
+        assertEquals("Hello",testObject.firstHalf("HelloThere"));
+        assertEquals("abc",testObject.firstHalf("abcdef"));
+        assertEquals("a",testObject.firstHalf("ab"));
+        assertEquals("",testObject.firstHalf(""));
+        assertEquals("01234",testObject.firstHalf("0123456789"));
+        assertEquals("kit",testObject.firstHalf("kitten"));
+        
+    }
+}
