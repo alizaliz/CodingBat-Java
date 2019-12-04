1f84c7314573ca5e8f9199774da0a4a2310a8330
Zain Ali <zain_ali8@hotmail.com>
2019-12-05 10:43:40 +1300

    Minor formatting fix

--- java-exercises/src/main/java/codingbat/Functional1.java
+++ java-exercises/src/main/java/codingbat/Functional1.java
@@ -14,7 +14,7 @@ import java.util.List;
       * 
       * doubling([1, 2, 3]) → [2, 4, 6]
       * doubling([6, 8, 6, 8, -1]) → [12, 16, 12, 16, -2]
-      *doubling([]) → []
+      * doubling([]) → []
     */
     public List<Integer> doubling(List<Integer> nums) {
         nums.replaceAll(n -> n*2);
