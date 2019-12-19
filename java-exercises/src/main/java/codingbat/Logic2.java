349c301eb74c046444b7f0d747c92c4b8916e49e
Zain Ali <zain_ali8@hotmail.com>
2019-12-19 20:16:08 +1300

    Created Logic2 branch, add one exercise.

---
+++ java-exercises/src/main/java/codingbat/Logic2.java
@@ -0,0 +1,33 @@
+package codingbat;
+
+/**
+ * Medium boolean logic puzzles -- if else && || !
+ */
+public final class Logic2 {
+
+    /**
+     * We want to make a row of bricks that is goal inches long. We have a number of
+     * small bricks (1 inch each) and big bricks (5 inches each). Return true if it
+     * is possible to make the goal by choosing from the given bricks. This is a
+     * little harder than it looks and can be done without any loops. See also:
+     * Introduction to MakeBricks
+     * 
+     * makeBricks(3, 1, 8) → true makeBricks(3, 1, 9) → false makeBricks(3, 2, 10) →
+     * true
+     */
+    public boolean makeBricks(int small, int big, int goal) {
+        if (small == goal || 5 * big == goal) {
+            return true;
+        }
+
+        int remainder = goal % 5;
+        int required = (goal - remainder) / 5;
+
+        if ((required <= big && (goal - (5 * required)) <= small)
+                || ((goal - big * 5) >= 0 && (goal - big * 5) <= small)) {
+            return true;
+        }
+        return false;
+    }
+
+}
---
+++ java-exercises/src/test/java/codingbat/Logic2Test.java
@@ -0,0 +1,50 @@
+package codingbat;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+
+public final class Logic2Test {
+
+    private Logic2 logic2testObject;
+
+    @Before
+    public void setUp() {
+        logic2testObject = new Logic2();
+    }
+
+    @Test
+    public void makeBricksTest() {
+        assertTrue(logic2testObject.makeBricks(3, 1, 8));
+        assertFalse(logic2testObject.makeBricks(3, 1, 9));
+        assertTrue(logic2testObject.makeBricks(3, 2, 10));
+        assertTrue(logic2testObject.makeBricks(3, 2, 8));
+        assertFalse(logic2testObject.makeBricks(3, 2, 9));
+        assertTrue(logic2testObject.makeBricks(6, 1, 11));
+        assertFalse(logic2testObject.makeBricks(6, 0, 11));
+        assertTrue(logic2testObject.makeBricks(1, 4, 11));
+        assertTrue(logic2testObject.makeBricks(0, 3, 10));
+        assertFalse(logic2testObject.makeBricks(1, 4, 12));
+        assertTrue(logic2testObject.makeBricks(3, 1, 7));
+        assertFalse(logic2testObject.makeBricks(1, 1, 7));
+        assertTrue(logic2testObject.makeBricks(2, 1, 7));
+        assertTrue(logic2testObject.makeBricks(7, 1, 11));
+        assertTrue(logic2testObject.makeBricks(7, 1, 8));
+        assertFalse(logic2testObject.makeBricks(7, 1, 13));
+        assertTrue(logic2testObject.makeBricks(43, 1, 46));
+        assertFalse(logic2testObject.makeBricks(40, 1, 46));
+        assertTrue(logic2testObject.makeBricks(40, 2, 47));
+        assertTrue(logic2testObject.makeBricks(40, 2, 50));
+        assertFalse(logic2testObject.makeBricks(40, 2, 52));
+        assertFalse(logic2testObject.makeBricks(22, 2, 33));
+        assertTrue(logic2testObject.makeBricks(0, 2, 10));
+        assertTrue(logic2testObject.makeBricks(1000000, 1000, 1000100));
+        assertFalse(logic2testObject.makeBricks(2, 1000000, 100003));
+        assertTrue(logic2testObject.makeBricks(20, 0, 19));
+        assertFalse(logic2testObject.makeBricks(20, 0, 21));
+        assertFalse(logic2testObject.makeBricks(20, 4, 51));
+        assertTrue(logic2testObject.makeBricks(20, 4, 39));
+    }
+
+}
