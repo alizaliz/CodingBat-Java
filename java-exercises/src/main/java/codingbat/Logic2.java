8609bea5ba217acb64bc90abda12256fa3a619dc
Zain Ali <zain_ali8@hotmail.com>
2019-12-21 11:35:27 +1300

    Add last seven test

--- java-exercises/src/main/java/codingbat/Logic2.java
+++ java-exercises/src/main/java/codingbat/Logic2.java
@@ -52,4 +52,147 @@ public final class Logic2 {
         return sum;
     }
 
+    /**
+     * Given 3 int values, a b c, return their sum. However, if one of the values is
+     * 13 then it does not count towards the sum and values to its right do not
+     * count. So for example, if b is 13, then both b and c do not count.
+     * 
+     * luckySum(1, 2, 3) → 6 luckySum(1, 2, 13) → 3 luckySum(1, 13, 3) → 1
+     */
+    public int luckySum(int a, int b, int c) {
+        int sum = 0;
+        if (a != 13) {
+            sum += a;
+            if (b != 13) {
+                sum += b;
+                if (c != 13) {
+                    sum += c;
+                }
+            }
+        }
+        return sum;
+    }
+
+    /**
+     * Given 3 int values, a b c, return their sum. However, if any of the values is
+     * a teen -- in the range 13..19 inclusive -- then that value counts as 0,
+     * except 15 and 16 do not count as a teens. Write a separate helper "public int
+     * fixTeen(int n) {"that takes in an int value and returns that value fixed for
+     * the teen rule. In this way, you avoid repeating the teen code 3 times (i.e.
+     * "decomposition"). Define the helper below and at the same indent level as the
+     * main noTeenSum().
+     * 
+     * noTeenSum(1, 2, 3) → 6 noTeenSum(2, 13, 1) → 3 noTeenSum(2, 1, 14) → 3
+     */
+    public int noTeenSum(int a, int b, int c) {
+        return fixTeen(a) + fixTeen(b) + fixTeen(c);
+    }
+
+    public int fixTeen(int n) {
+        if (n >= 13 && n <= 19) {
+            if (!(n == 15 || n == 16)) {
+                n = 0;
+            }
+        }
+
+        return n;
+    }
+
+    /**
+     * For this problem, we'll round an int value up to the next multiple of 10 if
+     * its rightmost digit is 5 or more, so 15 rounds up to 20. Alternately, round
+     * down to the previous multiple of 10 if its rightmost digit is less than 5, so
+     * 12 rounds down to 10. Given 3 ints, a b c, return the sum of their rounded
+     * values. To avoid code repetition, write a separate helper "public int
+     * round10(int num) {" and call it 3 times. Write the helper entirely below and
+     * at the same indent level as roundSum().
+     * 
+     * roundSum(16, 17, 18) → 60 roundSum(12, 13, 14) → 30 roundSum(6, 4, 4) → 10
+     */
+    public int roundSum(int a, int b, int c) {
+        return round10(a) + round10(b) + round10(c);
+    }
+
+    public int round10(int num) {
+        if (num % 10 >= 5) {
+            return ((num / 10) + 1) * 10;
+        }
+        return (num / 10) * 10;
+    }
+
+    /**
+     * Given three ints, a b c, return true if one of b or c is "close" (differing
+     * from a by at most 1), while the other is "far", differing from both other
+     * values by 2 or more. Note: Math.abs(num) computes the absolute value of a
+     * number.
+     * 
+     * closeFar(1, 2, 10) → true closeFar(1, 2, 3) → false closeFar(4, 1, 3) → true
+     */
+    public boolean closeFar(int a, int b, int c) {
+        return isClose(b, a) && isFar(c, a, b) || isClose(c, a) && isFar(b, a, c);
+    }
+
+    public boolean isClose(int test, int comp) {
+        return Math.abs(test - comp) <= 1;
+    }
+
+    public boolean isFar(int test, int compA, int compB) {
+        return Math.abs(test - compA) >= 2 && Math.abs(test - compB) >= 2;
+    }
+
+    /**
+     * Given 2 int values greater than 0, return whichever value is nearest to 21
+     * without going over. Return 0 if they both go over.
+     * 
+     * blackjack(19, 21) → 21 blackjack(21, 19) → 21 blackjack(19, 22) → 19
+     */
+    public int blackjack(int a, int b) {
+        int ret = 0;
+        if (21 - a > -1)
+            ret = a;
+        if (21 - b > -1) {
+            if (!(21 - a > -1 && 21 - b > 21 - a))
+                ret = b;
+        }
+
+        return ret;
+    }
+
+    /**
+     * Given three ints, a b c, one of them is small, one is medium and one is
+     * large. Return true if the three values are evenly spaced, so the difference
+     * between small and medium is the same as the difference between medium and
+     * large.
+     * 
+     * evenlySpaced(2, 4, 6) → true evenlySpaced(4, 6, 2) → true evenlySpaced(4, 6,
+     * 3) → false
+     */
+    public boolean evenlySpaced(int a, int b, int c) {
+        if (a == b && b == c)
+            return true;
+        if (a == b || b == c || a == c)
+            return false;
+        return (Math.abs(a - b) == Math.abs(a - c)) || (Math.abs(b - c) == Math.abs(a - b))
+                || (Math.abs(b - c) == Math.abs(a - c));
+    }
+
+    /**
+     * We want make a package of goal kilos of chocolate. We have small bars (1 kilo
+     * each) and big bars (5 kilos each). Return the number of small bars to use,
+     * assuming we always use big bars before small bars. Return -1 if it can't be
+     * done.
+     * 
+     * makeChocolate(4, 1, 9) → 4 makeChocolate(4, 1, 10) → -1 makeChocolate(4, 1,
+     * 7) → 2
+     */
+    public int makeChocolate(int small, int big, int goal) {
+        int required = 0;
+        required = goal % 5;
+        if (big - goal / 5 < 0) {
+            required += 5 * Math.abs(big - goal / 5);
+        }
+        if (required > small)
+            return -1;
+        return required;
+    }
 }
--- java-exercises/src/test/java/codingbat/Logic2Test.java
+++ java-exercises/src/test/java/codingbat/Logic2Test.java
@@ -48,7 +48,7 @@ public final class Logic2Test {
     }
 
     @Test
-    public void LoneSumTest() {
+    public void loneSumTest() {
         assertEquals(6, logic2testObject.loneSum(1, 2, 3));
         assertEquals(2, logic2testObject.loneSum(3, 2, 3));
         assertEquals(0, logic2testObject.loneSum(3, 3, 3));
@@ -60,4 +60,143 @@ public final class Logic2Test {
         assertEquals(3, logic2testObject.loneSum(1, 3, 1));
     }
 
+    @Test
+    public void luckySumTest() {
+        assertEquals(6, logic2testObject.luckySum(1, 2, 3));
+        assertEquals(3, logic2testObject.luckySum(1, 2, 13));
+        assertEquals(1, logic2testObject.luckySum(1, 13, 3));
+        assertEquals(1, logic2testObject.luckySum(1, 13, 13));
+        assertEquals(13, logic2testObject.luckySum(6, 5, 2));
+        assertEquals(0, logic2testObject.luckySum(13, 2, 3));
+        assertEquals(0, logic2testObject.luckySum(13, 2, 13));
+        assertEquals(0, logic2testObject.luckySum(13, 13, 2));
+        assertEquals(13, logic2testObject.luckySum(9, 4, 13));
+        assertEquals(8, logic2testObject.luckySum(8, 13, 2));
+        assertEquals(10, logic2testObject.luckySum(7, 2, 1));
+        assertEquals(6, logic2testObject.luckySum(3, 3, 13));
+    }
+
+    @Test
+    public void noTeenSumTest() {
+        assertEquals(6, logic2testObject.noTeenSum(1, 2, 3));
+        assertEquals(3, logic2testObject.noTeenSum(2, 13, 1));
+        assertEquals(3, logic2testObject.noTeenSum(2, 1, 14));
+        assertEquals(18, logic2testObject.noTeenSum(2, 1, 15));
+        assertEquals(19, logic2testObject.noTeenSum(2, 1, 16));
+        assertEquals(3, logic2testObject.noTeenSum(2, 1, 17));
+        assertEquals(3, logic2testObject.noTeenSum(17, 1, 2));
+        assertEquals(19, logic2testObject.noTeenSum(2, 15, 2));
+        assertEquals(16, logic2testObject.noTeenSum(16, 17, 18));
+        assertEquals(0, logic2testObject.noTeenSum(17, 18, 19));
+        assertEquals(32, logic2testObject.noTeenSum(15, 16, 1));
+        assertEquals(30, logic2testObject.noTeenSum(15, 15, 19));
+        assertEquals(31, logic2testObject.noTeenSum(15, 19, 16));
+        assertEquals(5, logic2testObject.noTeenSum(5, 17, 18));
+        assertEquals(16, logic2testObject.noTeenSum(17, 18, 16));
+        assertEquals(0, logic2testObject.noTeenSum(17, 19, 18));
+    }
+
+    @Test
+    public void roundSumTest() {
+        assertEquals(60, logic2testObject.roundSum(16, 17, 18));
+        assertEquals(30, logic2testObject.roundSum(12, 13, 14));
+        assertEquals(10, logic2testObject.roundSum(6, 4, 4));
+        assertEquals(20, logic2testObject.roundSum(4, 6, 5));
+        assertEquals(10, logic2testObject.roundSum(4, 4, 6));
+        assertEquals(10, logic2testObject.roundSum(9, 4, 4));
+        assertEquals(0, logic2testObject.roundSum(0, 0, 1));
+        assertEquals(10, logic2testObject.roundSum(0, 9, 0));
+        assertEquals(40, logic2testObject.roundSum(10, 10, 19));
+        assertEquals(90, logic2testObject.roundSum(20, 30, 40));
+        assertEquals(100, logic2testObject.roundSum(45, 21, 30));
+        assertEquals(60, logic2testObject.roundSum(23, 11, 26));
+        assertEquals(70, logic2testObject.roundSum(23, 24, 25));
+        assertEquals(80, logic2testObject.roundSum(25, 24, 25));
+        assertEquals(70, logic2testObject.roundSum(23, 24, 29));
+        assertEquals(70, logic2testObject.roundSum(11, 24, 36));
+        assertEquals(90, logic2testObject.roundSum(24, 36, 32));
+        assertEquals(50, logic2testObject.roundSum(14, 12, 26));
+        assertEquals(40, logic2testObject.roundSum(12, 10, 24));
+    }
+
+    @Test
+    public void closeFarTest() {
+        assertTrue(logic2testObject.closeFar(1, 2, 10));
+        assertFalse(logic2testObject.closeFar(1, 2, 3));
+        assertTrue(logic2testObject.closeFar(4, 1, 3));
+        assertFalse(logic2testObject.closeFar(4, 5, 3));
+        assertFalse(logic2testObject.closeFar(4, 3, 5));
+        assertTrue(logic2testObject.closeFar(-1, 10, 0));
+        assertTrue(logic2testObject.closeFar(0, -1, 10));
+        assertTrue(logic2testObject.closeFar(10, 10, 8));
+        assertFalse(logic2testObject.closeFar(10, 8, 9));
+        assertFalse(logic2testObject.closeFar(8, 9, 10));
+        assertFalse(logic2testObject.closeFar(8, 9, 7));
+        assertTrue(logic2testObject.closeFar(8, 6, 9));
+    }
+
+    @Test
+    public void blackjackyTest() {
+        assertEquals(21, logic2testObject.blackjack(19, 21));
+        assertEquals(21, logic2testObject.blackjack(21, 19));
+        assertEquals(19, logic2testObject.blackjack(19, 22));
+        assertEquals(19, logic2testObject.blackjack(22, 19));
+        assertEquals(0, logic2testObject.blackjack(22, 50));
+        assertEquals(0, logic2testObject.blackjack(22, 22));
+        assertEquals(1, logic2testObject.blackjack(33, 1));
+        assertEquals(2, logic2testObject.blackjack(1, 2));
+        assertEquals(0, logic2testObject.blackjack(34, 33));
+        assertEquals(19, logic2testObject.blackjack(17, 19));
+        assertEquals(18, logic2testObject.blackjack(18, 17));
+        assertEquals(16, logic2testObject.blackjack(16, 23));
+        assertEquals(4, logic2testObject.blackjack(3, 4));
+        assertEquals(3, logic2testObject.blackjack(3, 2));
+        assertEquals(21, logic2testObject.blackjack(21, 20));
+    }
+
+    @Test
+    public void evenlySpace() {
+        assertTrue(logic2testObject.evenlySpaced(2, 4, 6));
+        assertTrue(logic2testObject.evenlySpaced(4, 6, 2));
+        assertFalse(logic2testObject.evenlySpaced(4, 6, 3));
+        assertTrue(logic2testObject.evenlySpaced(6, 2, 4));
+        assertFalse(logic2testObject.evenlySpaced(6, 2, 8));
+        assertTrue(logic2testObject.evenlySpaced(2, 2, 2));
+        assertFalse(logic2testObject.evenlySpaced(2, 2, 3));
+        assertTrue(logic2testObject.evenlySpaced(9, 10, 11));
+        assertTrue(logic2testObject.evenlySpaced(10, 9, 11));
+        assertFalse(logic2testObject.evenlySpaced(10, 9, 9));
+        assertFalse(logic2testObject.evenlySpaced(2, 4, 4));
+        assertFalse(logic2testObject.evenlySpaced(2, 2, 4));
+        assertFalse(logic2testObject.evenlySpaced(3, 6, 12));
+        assertFalse(logic2testObject.evenlySpaced(12, 3, 6));
+    }
+
+    @Test
+    public void makeChocolateTest() {
+        assertEquals(4, logic2testObject.makeChocolate(4, 1, 9));
+        assertEquals(-1, logic2testObject.makeChocolate(4, 1, 10));
+        assertEquals(2, logic2testObject.makeChocolate(4, 1, 7));
+        assertEquals(2, logic2testObject.makeChocolate(6, 2, 7));
+        assertEquals(0, logic2testObject.makeChocolate(4, 1, 5));
+        assertEquals(4, logic2testObject.makeChocolate(4, 1, 4));
+        assertEquals(4, logic2testObject.makeChocolate(5, 4, 9));
+        assertEquals(3, logic2testObject.makeChocolate(9, 3, 18));
+        assertEquals(-1, logic2testObject.makeChocolate(3, 1, 9));
+        assertEquals(-1, logic2testObject.makeChocolate(1, 2, 7));
+        assertEquals(1, logic2testObject.makeChocolate(1, 2, 6));
+        assertEquals(0, logic2testObject.makeChocolate(1, 2, 5));
+        assertEquals(5, logic2testObject.makeChocolate(6, 1, 10));
+        assertEquals(6, logic2testObject.makeChocolate(6, 1, 11));
+        assertEquals(-1, logic2testObject.makeChocolate(6, 1, 12));
+        assertEquals(-1, logic2testObject.makeChocolate(6, 1, 13));
+        assertEquals(0, logic2testObject.makeChocolate(6, 2, 10));
+        assertEquals(1, logic2testObject.makeChocolate(6, 2, 11));
+        assertEquals(2, logic2testObject.makeChocolate(6, 2, 12));
+        assertEquals(50, logic2testObject.makeChocolate(60, 100, 550));
+        assertEquals(6, logic2testObject.makeChocolate(1000, 1000000, 5000006));
+        assertEquals(7, logic2testObject.makeChocolate(7, 1, 12));
+        assertEquals(-1, logic2testObject.makeChocolate(7, 1, 13));
+        assertEquals(3, logic2testObject.makeChocolate(7, 2, 13));
+    }
 }
