7dc22437ed5c214ecccaceb72bd8a2d6e227ba1c
Zain Ali <zain_ali8@hotmail.com>
2020-01-10 14:28:00 +1300

    Add one exercise.

--- java-exercises/src/test/java/codingbat/Arrays1Test.java
+++ java-exercises/src/test/java/codingbat/Arrays1Test.java
@@ -1,7 +1,6 @@
 package codingbat;
 
 import org.junit.Test;
-import org.junit.runner.notification.RunListener.ThreadSafe;
 
 import static org.junit.Assert.*;
 
@@ -312,5 +311,21 @@ public final class Arrays1Test {
         assertFalse(arrays1testObject.unlucky1(new int[] { 2, 3, 2, 1 }));
         assertTrue(arrays1testObject.unlucky1(new int[] { 2, 3, 1, 3 }));
         assertTrue(arrays1testObject.unlucky1(new int[] { 1, 2, 3, 4, 1, 3 }));
+    }
+
+    @Test
+    public void make2Test() {
+        assertArrayEquals(new int[] { 4, 5 }, arrays1testObject.make2(new int[] { 4, 5 }, new int[] { 1, 2, 3 }));
+        assertArrayEquals(new int[] { 4, 1 }, arrays1testObject.make2(new int[] { 4 }, new int[] { 1, 2, 3 }));
+        assertArrayEquals(new int[] { 1, 2 }, arrays1testObject.make2(new int[] {}, new int[] { 1, 2 }));
+        assertArrayEquals(new int[] { 1, 2 }, arrays1testObject.make2(new int[] { 1, 2 }, new int[] {}));
+        assertArrayEquals(new int[] { 3, 1 }, arrays1testObject.make2(new int[] { 3 }, new int[] { 1, 2, 3 }));
+        assertArrayEquals(new int[] { 3, 1 }, arrays1testObject.make2(new int[] { 3 }, new int[] { 1 }));
+        assertArrayEquals(new int[] { 3, 1 }, arrays1testObject.make2(new int[] { 3, 1, 4 }, new int[] {}));
+        assertArrayEquals(new int[] { 1, 1 }, arrays1testObject.make2(new int[] { 1 }, new int[] { 1 }));
+        assertArrayEquals(new int[] { 1, 2 }, arrays1testObject.make2(new int[] { 1, 2, 3 }, new int[] { 7, 8 }));
+        assertArrayEquals(new int[] { 7, 8 }, arrays1testObject.make2(new int[] { 7, 8 }, new int[] { 1, 2, 3 }));
+        assertArrayEquals(new int[] { 7, 1 }, arrays1testObject.make2(new int[] { 7 }, new int[] { 1, 2, 3 }));
+        assertArrayEquals(new int[] { 5, 4 }, arrays1testObject.make2(new int[] { 5, 4 }, new int[] { 2, 3, 7 }));
     }
 }
