ddaab9ca4f4b41371db3a7a7e64265beec47d5b8
Zain Ali <zain_ali8@hotmail.com>
2020-01-12 22:17:18 +1300

    Create Functional 2 classes.

---
+++ java-exercises/src/main/java/codingbat/Functional2.java
@@ -0,0 +1,12 @@
+package codingbat;
+
+import java.util.List;
+
+/**
+ * Functional filtering and mapping operations on lists with lambdas. See the
+ * Java Functional Filtering page for an introduction. The main computation for
+ * each of these problems can be done with 1 or 2 lines of lambda code.
+ */
+public final class Functional2 {
+
+}
---
+++ java-exercises/src/test/java/codingbat/Functional2Test.java
@@ -0,0 +1,20 @@
+package codingbat;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+
+/**
+ * Test cases for Functional1 exercises
+ */
+public class Functional2Test {
+
+    private Functional2 functional2TestObject;
+
+    @Before
+    public void setUp() {
+        functional2TestObject = new Functional2();
+    }
+}
