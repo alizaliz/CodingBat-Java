a3af4c5651574bd2721542d1e7118f5852af8c57
Zain Ali <zain_ali8@hotmail.com>
2020-01-12 22:32:47 +1300

    Add first exercise.

--- java-exercises/src/main/java/codingbat/Functional2.java
+++ java-exercises/src/main/java/codingbat/Functional2.java
@@ -1,5 +1,6 @@
 package codingbat;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -8,5 +9,17 @@ import java.util.List;
  * each of these problems can be done with 1 or 2 lines of lambda code.
  */
 public final class Functional2 {
+
+    /**
+     * Given a list of integers, return a list of the integers, omitting any that
+     * are less than 0.
+     * 
+     * noNeg([1, -2]) → [1] noNeg([-3, -3, 3, 3]) → [3, 3] noNeg([-1, -1, -1]) → []
+     */
+    public List<Integer> noNeg(List<Integer> nums) {
+        List<Integer> ret = new ArrayList<Integer>(nums);
+        ret.removeIf(n -> n < 0);
+        return ret;
+    }
 
 }
--- java-exercises/src/test/java/codingbat/Functional2Test.java
+++ java-exercises/src/test/java/codingbat/Functional2Test.java
@@ -17,4 +17,16 @@ public class Functional2Test {
     public void setUp() {
         functional2TestObject = new Functional2();
     }
+
+    @Test
+    public void noNegTest() {
+        assertEquals(Arrays.asList(1), functional2TestObject.noNeg(Arrays.asList(1, -2)));
+        assertEquals(Arrays.asList(2, 2), functional2TestObject.noNeg(Arrays.asList(-3, -3, 3, 3)));
+        assertEquals(Arrays.asList(), functional2TestObject.noNeg(Arrays.asList(-1, -1, -1)));
+        assertEquals(Arrays.asList(), functional2TestObject.noNeg(Arrays.asList()));
+        assertEquals(Arrays.asList(0, 1, 2), functional2TestObject.noNeg(Arrays.asList(0, 1, 2)));
+        assertEquals(Arrays.asList(3, 1, 4), functional2TestObject.noNeg(Arrays.asList(3, -10, 1, -1, 4, -400)));
+        assertEquals(Arrays.asList(3, 1, 5),
+                functional2TestObject.noNeg(Arrays.asList(-1, 3, 1, -1, -10, -100, -111, 5)));
+    }
 }
