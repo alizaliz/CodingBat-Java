2d9d5cd7c389482ca2a383e0fcdd6dbcd30515d6
Zain Ali <zain_ali8@hotmail.com>
2020-01-19 13:43:57 +1300

    Add wordLen exercise.

--- java-exercises/src/test/java/codingbat/Map2Test.java
+++ java-exercises/src/test/java/codingbat/Map2Test.java
@@ -27,4 +27,15 @@ public class Map2Test {
         assertEquals(Map.of("c", 0), map2TestObject.word0(new String[] { "c", "c", "c", "c" }));
         assertEquals(Map.of(), map2TestObject.word0(new String[] {}));
     }
+
+    @Test
+    public void wordLenTest() {
+        assertEquals(Map.of("bb", 2, "a", 1), map2TestObject.wordLen(new String[] { "a", "bb", "a", "bb" }));
+        assertEquals(Map.of("that", 4, "and", 3, "this", 4),
+                map2TestObject.wordLen(new String[] { "this", "and", "that", "and" }));
+        assertEquals(Map.of("code", 4, "bug", 3),
+                map2TestObject.wordLen(new String[] { "code", "code", "code", "bug" }));
+        assertEquals(Map.of(), map2TestObject.wordLen(new String[] {}));
+        assertEquals(Map.of("z", 1), map2TestObject.wordLen(new String[] { "z" }));
+    }
 }
