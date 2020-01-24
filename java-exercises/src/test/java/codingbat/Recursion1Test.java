e73f27ccf4ae3a01eb0a4fe33ef0c40ed4315dc3
Zain Ali <zain_ali8@hotmail.com>
2020-01-25 11:14:31 +1300

    Add factorial exercise.

--- java-exercises/src/test/java/codingbat/Recursion1Test.java
+++ java-exercises/src/test/java/codingbat/Recursion1Test.java
@@ -17,4 +17,17 @@ public class Recursion1Test {
         recursion1TestObject = new Recursion1();
     }
 
+    @Test
+    public void factoralTest() {
+        assertEquals(1, recursion1TestObject.factorial(1));
+        assertEquals(2, recursion1TestObject.factorial(2));
+        assertEquals(6, recursion1TestObject.factorial(3));
+        assertEquals(24, recursion1TestObject.factorial(4));
+        assertEquals(120, recursion1TestObject.factorial(5));
+        assertEquals(720, recursion1TestObject.factorial(6));
+        assertEquals(5040, recursion1TestObject.factorial(7));
+        assertEquals(40320, recursion1TestObject.factorial(8));
+        assertEquals(479001600, recursion1TestObject.factorial(12));
+    }
+
 }
