7ab50772ca55764335626a9bc4569bb2625cdd00
Zain Ali <zain_ali8@hotmail.com>
2020-01-25 11:07:46 +1300

    Create Recursion1 classes.

---
+++ java-exercises/src/main/java/codingbat/Recursion1.java
@@ -0,0 +1,12 @@
+package codingbat;
+
+/**
+ * Basic recursion problems. Recursion strategy: first test for one or two base
+ * cases that are so simple, the answer can be returned immediately. Otherwise,
+ * make a recursive a call for a smaller case (that is, a case which is a step
+ * towards the base case). Assume that the recursive call works correctly, and
+ * fix up what it returns to make the answer.
+ */
+public final class Recursion1 {
+    
+}
---
+++ java-exercises/src/test/java/codingbat/Recursion1Test.java
@@ -0,0 +1,20 @@
+package codingbat;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+
+/**
+ * Test cases for Recursion1 exercises
+ */
+public class Recursion1Test {
+
+    private Recursion1 recursion1TestObject;
+
+    @Before
+    public void setUp() {
+        recursion1TestObject = new Recursion1();
+    }
+
+}
