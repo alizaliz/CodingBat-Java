7405e128052963033ee7fa84e662765dcb1f30d4
Zain Ali <zain_ali8@hotmail.com>
2020-02-15 20:59:40 +1300

    Create Logic1 classes.

---
+++ java-exercises/src/main/java/codingbat/Logic1.java
@@ -0,0 +1,9 @@
+package codingbat;
+
+/**
+ * Basic boolean logic puzzles -- if else && || !.
+ */
+
+public final class Logic1 {
+    
+}
---
+++ java-exercises/src/test/java/codingbat/Logic1Test.java
@@ -0,0 +1,20 @@
+package codingbat;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+
+/**
+ * Test cases for Logic1 exercises
+ */
+public final class Logic1Test {
+
+    private Logic1 logic1testObject;
+
+    @Before
+    public void setUp() {
+        logic1testObject = new Logic1();
+    }
+
+}
