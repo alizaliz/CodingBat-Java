b2adaf2453bffd7fcff7795dac2e557c44d888ec
Zain Ali <zain_ali8@hotmail.com>
2020-02-15 21:03:45 +1300

    Add cigarParty exercise.

--- java-exercises/src/test/java/codingbat/Logic1Test.java
+++ java-exercises/src/test/java/codingbat/Logic1Test.java
@@ -17,4 +17,19 @@ public final class Logic1Test {
         logic1testObject = new Logic1();
     }
 
+    @Test
+    public void cigarPartyTest() {
+        assertFalse(logic1testObject.cigarParty(30, false));
+        assertTrue(logic1testObject.cigarParty(50, false));
+        assertTrue(logic1testObject.cigarParty(70, true));
+        assertFalse(logic1testObject.cigarParty(30, true));
+        assertTrue(logic1testObject.cigarParty(50, true));
+        assertTrue(logic1testObject.cigarParty(60, false));
+        assertFalse(logic1testObject.cigarParty(61, false));
+        assertTrue(logic1testObject.cigarParty(40, false));
+        assertFalse(logic1testObject.cigarParty(39, false));
+        assertTrue(logic1testObject.cigarParty(40, true));
+        assertFalse(logic1testObject.cigarParty(39, true));
+    }
+
 }
