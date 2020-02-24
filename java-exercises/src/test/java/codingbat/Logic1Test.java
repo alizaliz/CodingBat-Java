03a7749a542f83fd91a517eb25112ab8a6df2d78
Zain Ali <zain_ali8@hotmail.com>
2020-02-24 23:26:17 +1300

    Add inOrder exercise.

--- java-exercises/src/test/java/codingbat/Logic1Test.java
+++ java-exercises/src/test/java/codingbat/Logic1Test.java
@@ -368,4 +368,20 @@ public final class Logic1Test {
         assertTrue(logic1testObject.twoAsOne(3, 3, 0));
         assertFalse(logic1testObject.twoAsOne(3, 3, 2));
     }
+
+    @Test
+    public void inOrderTest() {
+        assertTrue(logic1testObject.inOrder(1, 2, 4, false));
+        assertFalse(logic1testObject.inOrder(1, 2, 1, false));
+        assertTrue(logic1testObject.inOrder(1, 1, 2, true));
+        assertFalse(logic1testObject.inOrder(3, 2, 4, false));
+        assertTrue(logic1testObject.inOrder(2, 3, 4, false));
+        assertTrue(logic1testObject.inOrder(3, 2, 4, true));
+        assertFalse(logic1testObject.inOrder(4, 2, 2, true));
+        assertFalse(logic1testObject.inOrder(4, 5, 2, true));
+        assertTrue(logic1testObject.inOrder(2, 4, 6, true));
+        assertTrue(logic1testObject.inOrder(7, 9, 10, false));
+        assertTrue(logic1testObject.inOrder(7, 5, 6, true));
+        assertFalse(logic1testObject.inOrder(7, 5, 4, true));
+    }
 }
