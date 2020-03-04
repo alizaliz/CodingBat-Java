a9b03801f7091a26972e1c41ac7b9e083972f682
Zain Ali <zain_ali8@hotmail.com>
2020-03-04 19:30:06 +1300

    Add groupSum6 exercise.

--- java-exercises/src/test/java/codingbat/Recursion2Test.java
+++ java-exercises/src/test/java/codingbat/Recursion2Test.java
@@ -33,4 +33,26 @@ public class Recursion2Test {
         assertTrue(recursion2TestObject.groupSum(0, new int[] { 10, 2, 2, 5 }, 15));
         assertTrue(recursion2TestObject.groupSum(0, new int[] { 10, 2, 2, 5 }, 9));
     }
+
+    @Test
+    public void groupSum6Test() {
+        assertTrue(recursion2TestObject.groupSum6(0, new int[] { 5, 6, 2 }, 8));
+        assertFalse(recursion2TestObject.groupSum6(0, new int[] { 5, 6, 2 }, 9));
+        assertFalse(recursion2TestObject.groupSum6(0, new int[] { 5, 6, 2 }, 7));
+        assertTrue(recursion2TestObject.groupSum6(0, new int[] { 1 }, 1));
+        assertFalse(recursion2TestObject.groupSum6(0, new int[] { 9 }, 1));
+        assertTrue(recursion2TestObject.groupSum6(0, new int[] {}, 0));
+        assertTrue(recursion2TestObject.groupSum6(0, new int[] { 3, 2, 4, 6 }, 8));
+        assertTrue(recursion2TestObject.groupSum6(0, new int[] { 6, 2, 4, 3 }, 8));
+        assertFalse(recursion2TestObject.groupSum6(0, new int[] { 5, 2, 4, 6 }, 9));
+        assertFalse(recursion2TestObject.groupSum6(0, new int[] { 6, 2, 4, 5 }, 9));
+        assertFalse(recursion2TestObject.groupSum6(0, new int[] { 3, 2, 4, 6 }, 3));
+        assertTrue(recursion2TestObject.groupSum6(0, new int[] { 1, 6, 2, 6, 4 }, 12));
+        assertTrue(recursion2TestObject.groupSum6(0, new int[] { 1, 6, 2, 6, 4 }, 13));
+        assertFalse(recursion2TestObject.groupSum6(0, new int[] { 1, 6, 2, 6, 4 }, 4));
+        assertFalse(recursion2TestObject.groupSum6(0, new int[] { 1, 6, 2, 6, 4 }, 9));
+        assertTrue(recursion2TestObject.groupSum6(0, new int[] { 1, 6, 2, 6, 5 }, 14));
+        assertTrue(recursion2TestObject.groupSum6(0, new int[] { 1, 6, 2, 6, 5 }, 15));
+        assertFalse(recursion2TestObject.groupSum6(0, new int[] { 1, 6, 2, 6, 5 }, 16));
+    }
 }
