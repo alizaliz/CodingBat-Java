c48037a33d5878d5a71c892d6bf0db802b0783fb
Zain Ali <zain_ali8@hotmail.com>
2020-03-05 23:07:02 +1300

    Add groupSum5 exercise.

--- java-exercises/src/test/java/codingbat/Recursion2Test.java
+++ java-exercises/src/test/java/codingbat/Recursion2Test.java
@@ -71,4 +71,23 @@ public class Recursion2Test {
         assertTrue(recursion2TestObject.groupNoAdj(0, new int[] { 9 }, 0));
         assertTrue(recursion2TestObject.groupNoAdj(0, new int[] { 5, 10, 4, 1 }, 11));
     }
+
+    @Test
+    public void groupSum5() {
+        assertTrue(recursion2TestObject.groupSum5(0, new int[] { 2, 5, 10, 4 }, 19));
+        assertTrue(recursion2TestObject.groupSum5(0, new int[] { 2, 5, 10, 4 }, 17));
+        assertFalse(recursion2TestObject.groupSum5(0, new int[] { 2, 5, 10, 4 }, 12));
+        assertFalse(recursion2TestObject.groupSum5(0, new int[] { 2, 5, 4, 10 }, 12));
+        assertFalse(recursion2TestObject.groupSum5(0, new int[] { 3, 5, 1 }, 4));
+        assertTrue(recursion2TestObject.groupSum5(0, new int[] { 3, 5, 1 }, 5));
+        assertTrue(recursion2TestObject.groupSum5(0, new int[] { 1, 3, 5 }, 5));
+        assertFalse(recursion2TestObject.groupSum5(0, new int[] { 3, 5, 1 }, 9));
+        assertFalse(recursion2TestObject.groupSum5(0, new int[] { 2, 5, 10, 4 }, 7));
+        assertTrue(recursion2TestObject.groupSum5(0, new int[] { 2, 5, 10, 4 }, 15));
+        assertFalse(recursion2TestObject.groupSum5(0, new int[] { 2, 5, 10, 4 }, 11));
+        assertTrue(recursion2TestObject.groupSum5(0, new int[] { 1 }, 1));
+        assertFalse(recursion2TestObject.groupSum5(0, new int[] { 9 }, 1));
+        assertTrue(recursion2TestObject.groupSum5(0, new int[] { 9 }, 0));
+        assertTrue(recursion2TestObject.groupSum5(0, new int[] {}, 0));
+    }
 }
