5b70644595d76f13787646dd54ce150d17d7db32
Zain Ali <zain_ali8@hotmail.com>
2020-03-24 23:49:46 +1300

    Add scoresSpecial exercise.

--- java-exercises/src/test/java/codingbat/AP1Test.java
+++ java-exercises/src/test/java/codingbat/AP1Test.java
@@ -232,4 +232,20 @@ public class AP1Test {
                 assertArrayEquals(new String[] { "ab", "ac" },
                                 ap1TestObject.wordsWithout(new String[] { "aa", "ab", "ac", "aa" }, "aa"));
         }
+
+        @Test
+        public void scoresSpecialTest() {
+                assertEquals(40, ap1TestObject.scoresSpecial(new int[] { 12, 10, 4 }, new int[] { 2, 20, 30 }));
+                assertEquals(40, ap1TestObject.scoresSpecial(new int[] { 20, 10, 4 }, new int[] { 2, 20, 10 }));
+                assertEquals(20, ap1TestObject.scoresSpecial(new int[] { 12, 11, 4 }, new int[] { 2, 20, 31 }));
+                assertEquals(50, ap1TestObject.scoresSpecial(new int[] { 1, 20, 2, 50 }, new int[] { 3, 4, 5 }));
+                assertEquals(50, ap1TestObject.scoresSpecial(new int[] { 3, 4, 5 }, new int[] { 1, 50, 2, 20 }));
+                assertEquals(50, ap1TestObject.scoresSpecial(new int[] { 10, 4, 20, 30 }, new int[] { 20 }));
+                assertEquals(50, ap1TestObject.scoresSpecial(new int[] { 10, 4, 20, 30 }, new int[] { 20 }));
+                assertEquals(50, ap1TestObject.scoresSpecial(new int[] { 10, 4, 20, 30 }, new int[] { 3, 20, 99 }));
+                assertEquals(60, ap1TestObject.scoresSpecial(new int[] { 10, 4, 20, 30 }, new int[] { 30, 20, 99 }));
+                assertEquals(0, ap1TestObject.scoresSpecial(new int[] {}, new int[] { 2 }));
+                assertEquals(20, ap1TestObject.scoresSpecial(new int[] {}, new int[] { 20 }));
+                assertEquals(40, ap1TestObject.scoresSpecial(new int[] { 14, 10, 4 }, new int[] { 4, 20, 30 }));
+        }
 }
