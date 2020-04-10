55fcae96bdb62475699c8a83fa1023d47415f626
Zain Ali <zain_ali8@hotmail.com>
2020-04-10 20:54:27 +1200

    Create Arrays2 classes.

---
+++ java-exercises/src/main/java/codingbat/Arrays2.java
@@ -0,0 +1,10 @@
+package codingbat;
+
+/**
+ * Medium array problems -- 1 loop. See the Java Arrays and Loops document for
+ * help. https://codingbat.com/doc/java-array-loops.html
+ */
+
+public class Arrays2 {
+
+}
---
+++ java-exercises/src/test/Arrays2Test.java
@@ -0,0 +1,21 @@
+package codingbat;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+
+/**
+ * Test cases for Arrays1 exercises
+ */
+public class Arrays2Test {
+
+    private Arrays1 arrays2testObject;
+
+    @Before
+    public void setUp() {
+        arrays2testObject = new Arrays2();
+    }
+
+}
