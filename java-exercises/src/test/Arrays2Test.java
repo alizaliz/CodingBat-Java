39d16d20722c24cfac11a656ce7f25c1791d7553
Zain Ali <zain_ali8@hotmail.com>
2020-04-10 20:58:13 +1200

    Add countEvens exercise.

--- java-exercises/src/test/Arrays2Test.java
+++ java-exercises/src/test/Arrays2Test.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+import java.beans.Transient;
+
 import org.junit.Before;
 
 /**
@@ -16,6 +18,18 @@ public class Arrays2Test {
     @Before
     public void setUp() {
         arrays2testObject = new Arrays2();
+    }
+
+    @Test
+    public void countEvensTest() {
+        assertEquals(3, arrays2testObject.countEvens(new int[] { 2, 1, 2, 3, 4 }));
+        assertEquals(3, arrays2testObject.countEvens(new int[] { 2, 2, 0 }));
+        assertEquals(0, arrays2testObject.countEvens(new int[] { 1, 3, 5 }));
+        assertEquals(0, arrays2testObject.countEvens(new int[] {}));
+        assertEquals(1, arrays2testObject.countEvens(new int[] { 11, 9, 0, 1 }));
+        assertEquals(2, arrays2testObject.countEvens(new int[] { 2, 11, 9, 0 }));
+        assertEquals(1, arrays2testObject.countEvens(new int[] { 2 }));
+        assertEquals(2, arrays2testObject.countEvens(new int[] { 2, 5, 12 }));
     }
 
 }
