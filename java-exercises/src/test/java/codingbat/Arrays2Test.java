7851631cc40d0d8cf5b82d01ea0ee51977d5d146
Zain Ali <zain_ali8@hotmail.com>
2020-04-14 19:34:50 +1200

    Correct file location.

--- java-exercises/src/test/java/codingbat/Arrays2Test.java
+++ java-exercises/src/test/java/codingbat/Arrays2Test.java
@@ -13,7 +13,7 @@ import org.junit.Before;
  */
 public class Arrays2Test {
 
-    private Arrays1 arrays2testObject;
+    private Arrays2 arrays2testObject;
 
     @Before
     public void setUp() {
@@ -48,4 +48,23 @@ public class Arrays2Test {
         assertEquals(3, arrays2testObject.bigDiff(new int[] { 7, 7, 6, 8, 5, 5, 6 }));
     }
 
+    @Test
+    public void centeredAverageTest() {
+        assertEquals(3, arrays2testObject.centeredAverage(new int[] { 1, 2, 3, 4, 100 }));
+        assertEquals(5, arrays2testObject.centeredAverage(new int[] { 1, 1, 5, 5, 10, 8, 7 }));
+        assertEquals(-3, arrays2testObject.centeredAverage(new int[] { -10, -4, -2, -4, -2, 0 }));
+        assertEquals(4, arrays2testObject.centeredAverage(new int[] { 5, 3, 4, 6, 2 }));
+        assertEquals(4, arrays2testObject.centeredAverage(new int[] { 5, 3, 4, 0, 100 }));
+        assertEquals(4, arrays2testObject.centeredAverage(new int[] { 100, 0, 5, 3, 4 }));
+        assertEquals(4, arrays2testObject.centeredAverage(new int[] { 4, 0, 100 }));
+        assertEquals(3, arrays2testObject.centeredAverage(new int[] { 0, 2, 3, 4, 100 }));
+        assertEquals(1, arrays2testObject.centeredAverage(new int[] { 1, 1, 100 }));
+        assertEquals(7, arrays2testObject.centeredAverage(new int[] { 7, 7, 7 }));
+        assertEquals(7, arrays2testObject.centeredAverage(new int[] { 1, 7, 8 }));
+        assertEquals(50, arrays2testObject.centeredAverage(new int[] { 1, 1, 99, 99 }));
+        assertEquals(50, arrays2testObject.centeredAverage(new int[] { 1000, 0, 1, 99 }));
+        assertEquals(4, arrays2testObject.centeredAverage(new int[] { 4, 4, 4, 4, 5 }));
+        assertEquals(4, arrays2testObject.centeredAverage(new int[] { 4, 4, 4, 1, 5 }));
+        assertEquals(6, arrays2testObject.centeredAverage(new int[] { 6, 4, 8, 12, 3 }));
+    }
 }
