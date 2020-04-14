7abe579c8e5490e34b973e4dfb567344766d0029
Zain Ali <zain_ali8@hotmail.com>
2020-04-14 19:36:10 +1200

    Add centeredAverage exercise.

--- java-exercises/src/main/java/codingbat/Arrays2.java
+++ java-exercises/src/main/java/codingbat/Arrays2.java
@@ -44,4 +44,47 @@ public class Arrays2 {
         return max - min;
     }
 
+    /**
+     * Return the "centered" average of an array of ints, which we'll say is the
+     * mean average of the values, except ignoring the largest and smallest values
+     * in the array. If there are multiple copies of the smallest value, ignore just
+     * one copy, and likewise for the largest value. Use int division to produce the
+     * final average. You may assume that the array is length 3 or more.
+     * 
+     * centeredAverage([1, 2, 3, 4, 100]) → 3 centeredAverage([1, 1, 5, 5, 10, 8,
+     * 7]) → 5 centeredAverage([-10, -4, -2, -4, -2, 0]) → -3
+     */
+    public int centeredAverage(int[] nums) {
+
+        int max = 0;
+        int min = 9999;
+
+        for (int i = 0; i < nums.length; i++) {
+            max = Math.max(max, nums[i]);
+            min = Math.min(min, nums[i]);
+
+        }
+
+        int minCount = 0;
+        int maxCount = 0;
+        int sum = 0;
+        float divider = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == min && minCount == 0) {
+                minCount++;
+                continue;
+            }
+
+            if (nums[i] == max && maxCount == 0) {
+                maxCount++;
+                continue;
+            }
+            sum += nums[i];
+            divider++;
+        }
+
+        return Math.round(sum / divider);
+    }
+
 }
