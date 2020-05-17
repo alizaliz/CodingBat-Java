162d2aae89eb19ad821d34b5cfba095e80552705
Zain Ali <zain_ali8@hotmail.com>
2020-05-17 19:56:35 +1200

    Add has12 exercise.

--- java-exercises/src/main/java/codingbat/Arrays2.java
+++ java-exercises/src/main/java/codingbat/Arrays2.java
@@ -394,4 +394,29 @@ public class Arrays2 {
         return false;
     }
 
+    /**
+     * Given an array of ints, return true if there is a 1 in the array with a 2
+     * somewhere later in the array.
+     * 
+     * has12([1, 3, 2]) → true has12([3, 1, 2]) → true has12([3, 1, 4, 5, 2]) → true
+     */
+    public boolean has12(int[] nums) {
+        int flag = 0;
+        for (int i = 0; i < nums.length; i++) {
+            switch (flag) {
+                case 0:
+                    if (nums[i] == 1)
+                        flag++;
+                    break;
+                case 1:
+                    if (nums[i] == 2)
+                        return true;
+                    break;
+                default:
+                    continue;
+            }
+        }
+        return false;
+    }
+
 }
--- java-exercises/src/test/java/codingbat/Arrays2Test.java
+++ java-exercises/src/test/java/codingbat/Arrays2Test.java
@@ -309,4 +309,25 @@ public class Arrays2Test {
         assertFalse(arrays2testObject.has77(new int[] { 1, 7 }));
         assertFalse(arrays2testObject.has77(new int[] { 7 }));
     }
+
+    @Test
+    public void has12Test() {
+        assertTrue(arrays2testObject.has12(new int[] { 1, 3, 2 }));
+        assertTrue(arrays2testObject.has12(new int[] { 3, 1, 2 }));
+        assertTrue(arrays2testObject.has12(new int[] { 3, 1, 4, 5, 2 }));
+        assertFalse(arrays2testObject.has12(new int[] { 3, 1, 4, 5, 6 }));
+        assertTrue(arrays2testObject.has12(new int[] { 3, 1, 4, 1, 6, 2 }));
+        assertTrue(arrays2testObject.has12(new int[] { 2, 1, 4, 1, 6, 2 }));
+        assertFalse(arrays2testObject.has12(new int[] { 2, 1, 4, 1, 6 }));
+        assertFalse(arrays2testObject.has12(new int[] { 1 }));
+        assertFalse(arrays2testObject.has12(new int[] { 2, 1, 3 }));
+        assertTrue(arrays2testObject.has12(new int[] { 2, 1, 3, 2 }));
+        assertFalse(arrays2testObject.has12(new int[] { 2 }));
+        assertFalse(arrays2testObject.has12(new int[] { 3, 2 }));
+        assertTrue(arrays2testObject.has12(new int[] { 3, 1, 3, 2 }));
+        assertFalse(arrays2testObject.has12(new int[] { 3, 5, 9 }));
+        assertFalse(arrays2testObject.has12(new int[] { 3, 5, 1 }));
+        assertFalse(arrays2testObject.has12(new int[] { 3, 2, 1 }));
+        assertTrue(arrays2testObject.has12(new int[] { 1, 2 }));
+    }
 }
