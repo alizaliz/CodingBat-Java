b774c5ff47a8f34932ee10c06610a0cc32793e1a
Zain Ali <zain_ali8@hotmail.com>
2020-08-15 21:53:34 +1200

    Add fix45 exercise.

--- java-exercises/src/main/java/codingbat/Arrays3.java
+++ java-exercises/src/main/java/codingbat/Arrays3.java
@@ -68,10 +68,44 @@ public class Arrays3 {
         return nums;
 
     }
+
     public int[] swapValues(int[] nums, int i, int j) {
         int temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
+
+        return nums;
+    }
+
+    /**
+     * (This is a slightly harder version of the fix34 problem.) Return an array
+     * that contains exactly the same numbers as the given array, but rearranged so
+     * that every 4 is immediately followed by a 5. Do not move the 4's, but every
+     * other number may move. The array contains the same number of 4's and 5's, and
+     * every 4 has a number after it that is not a 4. In this version, 5's may
+     * appear anywhere in the original array.
+     * 
+     * 
+     * fix45([5, 4, 9, 4, 9, 5]) → [9, 4, 5, 4, 5, 9] fix45([1, 4, 1, 5]) → [1, 4,
+     * 5, 1] fix45([1, 4, 1, 5, 5, 4, 1]) → [1, 4, 5, 1, 1, 4, 5]
+     */
+    public int[] fix45(int[] nums) {
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (i > 0 && nums[i - 1] == 4 && nums[i] != 5) {
+                int j = 0;
+                for (; j < nums.length; j++) {
+                    if (nums[j] == 5 && (j == 0 || nums[j - 1] != 4)) {
+                        break;
+                    }
+                }
+                int tmp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = tmp;
+            }
+
+        }
 
         return nums;
     }
--- java-exercises/src/test/java/codingbat/Arrays3Test.java
+++ java-exercises/src/test/java/codingbat/Arrays3Test.java
@@ -53,4 +53,28 @@ public class Arrays3Test {
         assertArrayEquals(new int[] { 3, 4, 1, 3, 4, 1 }, arrays3testObject.fix34(new int[] { 3, 1, 4, 3, 1, 4 }));
         assertArrayEquals(new int[] { 3, 4, 1, 3, 4, 1 }, arrays3testObject.fix34(new int[] { 3, 1, 1, 3, 4, 4 }));
     }
+
+    @Test
+    public void fix45Test() {
+        assertArrayEquals(new int[] { 9, 4, 5, 4, 5, 9 }, arrays3testObject.fix45(new int[] { 5, 4, 9, 4, 9, 5 }));
+        assertArrayEquals(new int[] { 1, 4, 5, 1 }, arrays3testObject.fix45(new int[] { 1, 4, 1, 5 }));
+        assertArrayEquals(new int[] { 1, 4, 5, 1, 1, 4, 5 },
+                arrays3testObject.fix45(new int[] { 1, 4, 1, 5, 5, 4, 1 }));
+        assertArrayEquals(new int[] { 4, 5, 4, 5, 9, 9, 4, 5, 9 },
+                arrays3testObject.fix45(new int[] { 4, 9, 4, 9, 5, 5, 4, 9, 5 }));
+        assertArrayEquals(new int[] { 1, 1, 4, 5, 4, 5 }, arrays3testObject.fix45(new int[] { 5, 5, 4, 1, 4, 1 }));
+        assertArrayEquals(new int[] { 4, 5, 2, 2 }, arrays3testObject.fix45(new int[] { 4, 2, 2, 5 }));
+        assertArrayEquals(new int[] { 4, 5, 4, 5, 2, 2 }, arrays3testObject.fix45(new int[] { 4, 2, 4, 2, 5, 5 }));
+        assertArrayEquals(new int[] { 4, 5, 4, 5, 2 }, arrays3testObject.fix45(new int[] { 4, 2, 4, 5, 5 }));
+        assertArrayEquals(new int[] { 1, 1, 1 }, arrays3testObject.fix45(new int[] { 1, 1, 1 }));
+        assertArrayEquals(new int[] { 4, 5 }, arrays3testObject.fix45(new int[] { 4, 5 }));
+        assertArrayEquals(new int[] { 1, 4, 5 }, arrays3testObject.fix45(new int[] { 5, 4, 1 }));
+        assertArrayEquals(new int[] {}, arrays3testObject.fix45(new int[] {}));
+        assertArrayEquals(new int[] { 1, 4, 5, 4, 5 }, arrays3testObject.fix45(new int[] { 5, 4, 5, 4, 1 }));
+        assertArrayEquals(new int[] { 4, 5, 4, 5, 1 }, arrays3testObject.fix45(new int[] { 4, 5, 4, 1, 5 }));
+        assertArrayEquals(new int[] { 3, 4, 5 }, arrays3testObject.fix45(new int[] { 3, 4, 5 }));
+        assertArrayEquals(new int[] { 4, 5, 1 }, arrays3testObject.fix45(new int[] { 4, 1, 5 }));
+        assertArrayEquals(new int[] { 1, 4, 5 }, arrays3testObject.fix45(new int[] { 5, 4, 1 }));
+        assertArrayEquals(new int[] { 2, 4, 5, 2 }, arrays3testObject.fix45(new int[] { 2, 4, 2, 5 }));
+    }
 }
