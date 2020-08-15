a753cc5b2ff13deb5592039063825006b62a7ed7
Zain Ali <zain_ali8@hotmail.com>
2020-08-15 21:47:17 +1200

    Add fix34 exercise.

--- java-exercises/src/test/java/codingbat/Arrays3Test.java
+++ java-exercises/src/test/java/codingbat/Arrays3Test.java
@@ -4,7 +4,6 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-
 import org.junit.Before;
 
 /**
@@ -32,5 +31,26 @@ public class Arrays3Test {
         assertEquals(2, arrays3testObject.maxSpan(new int[] { 3, 3 }));
         assertEquals(0, arrays3testObject.maxSpan(new int[] {}));
         assertEquals(1, arrays3testObject.maxSpan(new int[] { 1 }));
+    }
+
+    @Test
+    public void fix34Test() {
+        assertArrayEquals(new int[] { 1, 3, 4, 1 }, arrays3testObject.fix34(new int[] { 1, 3, 1, 4 }));
+        assertArrayEquals(new int[] { 1, 3, 4, 1, 1, 3, 4 },
+                arrays3testObject.fix34(new int[] { 1, 3, 1, 4, 4, 3, 1 }));
+        assertArrayEquals(new int[] { 3, 4, 2, 2 }, arrays3testObject.fix34(new int[] { 3, 2, 2, 4 }));
+        assertArrayEquals(new int[] { 3, 4, 3, 4, 2, 2 }, arrays3testObject.fix34(new int[] { 3, 2, 3, 2, 4, 4 }));
+        assertArrayEquals(new int[] { 2, 3, 4, 3, 4, 2, 2 },
+                arrays3testObject.fix34(new int[] { 2, 3, 2, 3, 2, 4, 4 }));
+        assertArrayEquals(new int[] { 5, 3, 4, 5, 5, 5, 5, 5, 3, 4, 3, 4 },
+                arrays3testObject.fix34(new int[] { 5, 3, 5, 4, 5, 4, 5, 4, 3, 5, 3, 5 }));
+        assertArrayEquals(new int[] { 3, 4, 1 }, arrays3testObject.fix34(new int[] { 3, 1, 4 }));
+        assertArrayEquals(new int[] { 3, 4, 1 }, arrays3testObject.fix34(new int[] { 3, 4, 1 }));
+        assertArrayEquals(new int[] { 1, 1, 1 }, arrays3testObject.fix34(new int[] { 1, 1, 1 }));
+        assertArrayEquals(new int[] { 1 }, arrays3testObject.fix34(new int[] { 1 }));
+        assertArrayEquals(new int[] {}, arrays3testObject.fix34(new int[] {}));
+        assertArrayEquals(new int[] { 7, 3, 4, 7, 7 }, arrays3testObject.fix34(new int[] { 7, 3, 7, 7, 4 }));
+        assertArrayEquals(new int[] { 3, 4, 1, 3, 4, 1 }, arrays3testObject.fix34(new int[] { 3, 1, 4, 3, 1, 4 }));
+        assertArrayEquals(new int[] { 3, 4, 1, 3, 4, 1 }, arrays3testObject.fix34(new int[] { 3, 1, 1, 3, 4, 4 }));
     }
 }
