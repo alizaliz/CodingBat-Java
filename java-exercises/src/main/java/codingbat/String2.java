14caa5db543d414c34063d9cf7f4615312859169
Zain Ali <zain_ali8@hotmail.com>
2020-08-17 09:59:48 +1200

    Create String2 classes.

---
+++ java-exercises/src/main/java/codingbat/String2.java
@@ -0,0 +1,10 @@
+package codingbat;
+
+/**
+ * Medium String problems -- 1 loop. See the Java String Help document for help
+ * with strings. https://codingbat.com/doc/string.html
+ */
+
+public class String2 {
+
+}
---
+++ java-exercises/src/test/java/codingbat/String2Test.java
@@ -0,0 +1,20 @@
+package codingbat;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+
+/**
+ * Test cases for String2 exercises
+ */
+public class String2Test {
+
+    private String2 string2testObject;
+
+    @Before
+    public void setUp() {
+        string2testObject = new String2();
+    }
+
+}
