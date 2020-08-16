40a96fb2f35b1add38db2631ed48c12fcd9abbd7
Zain Ali <zain_ali8@hotmail.com>
2020-08-17 09:45:51 +1200

    Add maxMIrror exercise.

--- java-exercises/src/main/java/codingbat/Arrays3.java
+++ java-exercises/src/main/java/codingbat/Arrays3.java
@@ -209,4 +209,41 @@ public class Arrays3 {
         return ret;
     }
 
+    /**
+     * We'll say that a "mirror" section in an array is a group of contiguous
+     * elements such that somewhere in the array, the same group appears in reverse
+     * order. For example, the largest mirror section in {1, 2, 3, 8, 9, 3, 2, 1} is
+     * length 3 (the {1, 2, 3} part). Return the size of the largest mirror section
+     * found in the given array.
+     * 
+     * 
+     * maxMirror([1, 2, 3, 8, 9, 3, 2, 1]) → 3 maxMirror([1, 2, 1, 4]) → 3
+     * maxMirror([7, 1, 2, 9, 7, 2, 1]) → 2
+     */
+    public int maxMirror(int[] nums) {
+
+        int maxMirror = 0, k = 0;
+        boolean mirrorStart = false;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            mirrorStart = false;
+            for (int j = nums.length - 1; j >= 0 && i + k < nums.length; j--) {
+                if (mirrorStart) {
+                    if (nums[i + k] == nums[j]) {
+                        k++;
+                    } else {
+                        mirrorStart = false;
+                    }
+                } else if (nums[i] == nums[j]) {
+                    k = 1;
+                    mirrorStart = true;
+                }
+                maxMirror = k > maxMirror ? k : maxMirror;
+            }
+        }
+
+        return maxMirror;
+    }
+
 }
--- java-exercises/src/test/java/codingbat/Arrays3Test.java
+++ java-exercises/src/test/java/codingbat/Arrays3Test.java
@@ -131,4 +131,23 @@ public class Arrays3Test {
         assertArrayEquals(new int[] { 1, 1, 2, 1, 2, 3, 1, 2, 3, 4, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 6 },
                 arrays3testObject.seriesUp(6));
     }
+
+    @Test
+    public void maxMirrorTest() {
+        assertEquals(3, arrays3testObject.maxMirror(new int[] { 1, 2, 3, 8, 9, 3, 2, 1 }));
+        assertEquals(3, arrays3testObject.maxMirror(new int[] { 1, 2, 1, 4 }));
+        assertEquals(2, arrays3testObject.maxMirror(new int[] { 7, 1, 2, 9, 7, 2, 1 }));
+        assertEquals(4, arrays3testObject.maxMirror(new int[] { 21, 22, 9, 8, 7, 6, 23, 24, 6, 7, 8, 9, 25, 7, 8, 9 }));
+        assertEquals(4, arrays3testObject.maxMirror(new int[] { 1, 2, 1, 20, 21, 1, 2, 1, 2, 23, 24, 2, 1, 2, 1, 25 }));
+        assertEquals(5, arrays3testObject.maxMirror(new int[] { 1, 2, 3, 2, 1 }));
+        assertEquals(2, arrays3testObject.maxMirror(new int[] { 1, 2, 3, 3, 8 }));
+        assertEquals(2, arrays3testObject.maxMirror(new int[] { 1, 2, 7, 8, 1, 7, 2 }));
+        assertEquals(3, arrays3testObject.maxMirror(new int[] { 1, 1, 1 }));
+        assertEquals(1, arrays3testObject.maxMirror(new int[] { 1 }));
+        assertEquals(0, arrays3testObject.maxMirror(new int[] {}));
+        assertEquals(3, arrays3testObject.maxMirror(new int[] { 9, 1, 1, 4, 2, 1, 1, 1 }));
+        assertEquals(7, arrays3testObject.maxMirror(new int[] { 5, 9, 9, 4, 5, 4, 9, 9, 2 }));
+        assertEquals(2, arrays3testObject.maxMirror(new int[] { 5, 9, 9, 6, 5, 4, 9, 9, 2 }));
+        assertEquals(3, arrays3testObject.maxMirror(new int[] { 5, 9, 1, 6, 5, 4, 1, 9, 5 }));
+    }
 }
