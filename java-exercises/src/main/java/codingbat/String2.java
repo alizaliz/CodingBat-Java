92cd1c39f191ca373ad6d9bb59aad862bc1069b4
Zain Ali <zain_ali8@hotmail.com>
2020-08-20 15:34:32 +1200

    Add doubleChar exercise.

--- java-exercises/src/main/java/codingbat/String2.java
+++ java-exercises/src/main/java/codingbat/String2.java
@@ -7,4 +7,22 @@ package codingbat;
 
 public class String2 {
 
+    /**
+     * Given a string, return a string where for every char in the original, there
+     * are two chars.
+     * 
+     * 
+     * doubleChar("The") → "TThhee" doubleChar("AAbb") → "AAAAbbbb"
+     * doubleChar("Hi-There") → "HHii--TThheerree"
+     */
+    public String doubleChar(String str) {
+        String ret = "";
+
+        for (int i = 0; i < str.length(); i++) {
+            ret += str.charAt(i);
+            ret += str.charAt(i);
+        }
+        return ret;
+    }
+
 }
--- java-exercises/src/test/java/codingbat/String2Test.java
+++ java-exercises/src/test/java/codingbat/String2Test.java
@@ -17,4 +17,17 @@ public class String2Test {
         string2testObject = new String2();
     }
 
+    @Test
+    public void doubleCharTest() {
+        assertEquals("TThhee", string2testObject.doubleChar("The"));
+        assertEquals("AAAAbbbb", string2testObject.doubleChar("AAbb"));
+        assertEquals("HHii--TThheerree", string2testObject.doubleChar("Hi-There"));
+        assertEquals("WWoorrdd!!", string2testObject.doubleChar("Word!"));
+        assertEquals("!!!!", string2testObject.doubleChar("!!"));
+        assertEquals("", string2testObject.doubleChar(""));
+        assertEquals("aa", string2testObject.doubleChar("a"));
+        assertEquals("..", string2testObject.doubleChar("."));
+        assertEquals("aaaa", string2testObject.doubleChar("aa"));
+    }
+
 }
